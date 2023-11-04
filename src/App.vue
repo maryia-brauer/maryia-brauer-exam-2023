5ed3b5e2880756d063d865050fe77cc32422da1e
Maryia <maryia.brauer@gmail.com>
2023-11-04 20:19:17 +0200

    Task 13

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,26 @@
-<template>
-    <!--  HEADER  -->
-    <Header></Header>
-    <!--  HEADER END  -->
+import { useAuthStore } from '@/stores/auth.js'
 
-    <!--  BODY  -->
+<template>
+    <Header v-if="is_authenticated"></Header>
     <div id="section-body">
-        <nav id="nav-main" class="wrapper-navigation">
-            <ol>
-                <li>
-                    <a href="/">SONGS</a>
-                </li>
-                <li>
-                    <a href="/albums">ALBUMS</a>
-                </li>
-                <li>
-                    <a href="/about">ABOUT</a>
-                </li>
-            </ol>
-        </nav>
+        <NavigationComponent v-if="is_authenticated"></NavigationComponent>
         <router-view class="section-router"></router-view>
     </div>
-    <!--  BODY END  -->
-
-    <!--  PLAYER  -->
-    <div id="section-player">
+    <div id="section-player" v-if="is_authenticated">
         <AudioPlayer />
     </div>
-    <!--  PLAYER END  -->
 </template>
 
 <script>
-    import Header from '@/components/HeaderComponent.vue';
-    import AudioPlayer from '@/components/AudioPlayerComponent.vue';
-
-    export default {
-        components: {
-            Header,
-            AudioPlayer
-        }
+export default {
+    setup() {
+        const auth = useAuthStore()
+        return { is_authenticated: auth.is_authenticated }
+    },
+    components: {
+        NavigationComponent,
+        AudioPlayer
     }
+}
+
 </script>
