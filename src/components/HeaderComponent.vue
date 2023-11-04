6cf32ad31ac410b5fa91730557b5b47b4bfc3e90
Maryia <maryia.brauer@gmail.com>
2023-11-04 22:59:08 +0200

    Changed header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,41 @@
 <template>
     <div id="main-header" class="active">
-        <div class="wrapper-logo">
-            <img src="@/assets/logo.svg" alt="logo"/>
-            <h2>KRAKEN.FM</h2>
+      <div class="wrapper-logo">
+        <img src="@/assets/logo.svg" alt="logo" />
+        <h2>KRAKEN.FM</h2>
+      </div>
+      <div class="wrapper-profile">
+        <div class="section-user">
+          <span class="avatar"></span>
+          <h3>{{ fullName }}</h3>
         </div>
-        <div class="wrapper-profile">
-            <div class="section-user">
-                <span class="avatar"></span>
-                <h3 id="txt-full-name">NAME SURNAME</h3>
-            </div>
-            <button id="btn-logout">LOGOUT</button>
-        </div>
+        <button @click="logout">LOGOUT</button>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <style scoped lang="scss"></style>
+  
+  <script>
+  import { useAuthStore } from '@/stores/auth';
+  
+  export default {
+    data() {
+      return {
+        auth: useAuthStore(),
+      };
+    },
+    computed: {
+      fullName() {
+        const { user } = this.auth;
+        return `${user.name} ${user.surname}`;
+      },
+    },
+    methods: {
+      logout() {
+        this.auth.logout();
+      },
+    },
+  };
+  </script>
+  
